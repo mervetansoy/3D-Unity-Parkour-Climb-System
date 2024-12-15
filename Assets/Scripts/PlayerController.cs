0f82d20e0db36e4b39f3a7893c50f76b1756fc9e
Merve Tansoy <157800967+mervetansoy@users.noreply.github.com>
2024-12-15 18:24:03 +0300

    anim1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
    [SerializeField] float moveSpeed=5f;
    [SerializeField] float rotationSpeed=500f;
+   [Header("Ground Check Settings")]
+   [SerializeField] float groundCheckRadius=0.2f;
+   [SerializeField] Vector3 groundCheckOffset;
+   [SerializeField] LayerMask groundLayer;
+
+   bool isGrounded;
+   float ySpeed;
 
    Quaternion targetRotation;
 
    CameraController cameraController;
    Animator animator;
+   CharacterController characterController;
 
    private void Awake()
    {
     cameraController=Camera.main.GetComponent<CameraController>();
     animator = GetComponent<Animator>();
+    characterController=GetComponent<CharacterController>();
    }
 
    private void Update()
@@ -31,9 +41,25 @@ public class PlayerController : MonoBehaviour
 
     var moveD= cameraController.PlanerRotation * moveInput;
 
+    groundCheck();
+    Debug.Log("Ground="+isGrounded);
+    if (isGrounded)
+    {
+        ySpeed = -0.5f;
+    }
+    else
+    {
+        ySpeed +=Physics.gravity.y * Time.deltaTime;
+    }
+
+    var velocity = moveD* moveSpeed;
+    velocity.y=ySpeed;
+    
+    characterController.Move(velocity * Time.deltaTime);
+
     if(moveAmount > 0)
     {
-        transform.position += moveD* moveSpeed * Time.deltaTime;
+        
         transform.rotation = Quaternion.LookRotation(moveD);
     }
 
@@ -42,4 +68,16 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("MoveAnim", moveAmount, 0.2f, Time.deltaTime);
     
    }
+
+   void groundCheck()
+   {
+    isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+   }
+
+   private void OnDrawGizmosSelected()
+   {
+    Gizmos.color= new Color(0,1,0,0.5f);
+    Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
+   }
 }
+
