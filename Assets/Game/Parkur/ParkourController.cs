075cfbabc14119fc05e79873839527f28a95ad5c
Merve Tansoy <157800967+mervetansoy@users.noreply.github.com>
2024-12-23 18:15:23 +0300

    work

--- Assets/Game/Parkur/ParkourController.cs
+++ Assets/Game/Parkur/ParkourController.cs
@@ -37,47 +37,71 @@ public class ParkourController : MonoBehaviour
     }
    }
 
-   IEnumerator DoParkourAction(ParkourAction action)
-   {
-        inAction = true;
-        playerController.SetControl(false);
+ IEnumerator DoParkourAction(ParkourAction action)
+{
+    inAction = true;
+    playerController.SetControl(false);
 
-        animator.SetBool("mirrorAction",action.Mirror);
-        animator.CrossFade(action.AnimationName, 0.2f);
+    animator.SetBool("mirrorAction", action.Mirror);
+    animator.CrossFade(action.AnimationName, 0.2f);
+
+    // Animasyonun geçiş yapmasını bekle
+    yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName(action.AnimationName));
+
+    var animationState = animator.GetCurrentAnimatorStateInfo(0);
+    if (!animationState.IsName(action.AnimationName))
+    {
+        Debug.LogError("Yanlış animasyon oynatılıyor!");
+        yield break;
+    }
+
+    float timer = 0f;
+    while (timer <= animationState.length)
+    {
+        timer += Time.deltaTime;
+
+        if (action.RotateToObstacle)
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                action.TargetRotation,
+                playerController.RotationSpeed * Time.deltaTime
+            );
+
+        if (action.EnableTargetMatching)
+            MatchTarget(action);
+
         yield return null;
+    }
 
-        var animationState = animator.GetNextAnimatorStateInfo(0);
-        if(!animationState.IsName(action.AnimationName))
-        Debug.LogError("yanlis yaptin ");
+    yield return new WaitForSeconds(action.PositionActionDelay);
 
-        
+    playerController.SetControl(true);
+    inAction = false;
+}
 
-        float timer =0f;
-        while(timer <= animationState.length)
-        {
-            timer += Time.deltaTime;
 
-            if(action.RotateToObstacle)
-                transform.rotation =Quaternion.RotateTowards(transform.rotation,action.TargetRotation, playerController.RotationSpeed*Time.deltaTime);
-
-            if(action.EnableTargetMatching)
-                MatchTarget(action);
-
-            if(animator.IsInTransition(0) && timer >0.5f)
-            break;
-            
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(action.PositionActionDelay);
-
-        playerController.SetControl(true);
-        inAction = false;
-   }
 
    void MatchTarget(ParkourAction action)
-   {
+{
+    // Geçiş durumunda MatchTarget çağrısını yapma
+    if (animator.IsInTransition(0)) return;
+
+    // Doğru animasyon oynuyor mu?
+    var currentAnimation = animator.GetCurrentAnimatorStateInfo(0);
+    if (!currentAnimation.IsName(action.AnimationName)) return;
+
+    // Eğer hala MatchTarget aktifse, tekrar çağırmayı engelle
     if (animator.isMatchingTarget) return;
-    animator.MatchTarget(action.MatchPosition,transform.rotation,action.MatchBodyPart,new MatchTargetWeightMask(action.MatchPositionWeith,0),action.MatchStartTime,action.MatchTargetTime);
-   }
+
+    // MatchTarget çağrısı
+    animator.MatchTarget(
+        action.MatchPosition, 
+        transform.rotation, 
+        action.MatchBodyPart, 
+        new MatchTargetWeightMask(action.MatchPositionWeith, 0), 
+        action.MatchStartTime, 
+        action.MatchTargetTime
+    );
+}
+
 }
